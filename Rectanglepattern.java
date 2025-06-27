2cba7171a5fa2157dbdfec101cbe6da8bcb27113
Vaibhav1222 <95678002+Vaibhav1222@users.noreply.github.com>
2025-06-27 10:15:10 +0530

    Add files via upload

---
+++ Rectanglepattern.java
@@ -0,0 +1,10 @@
+class Rectanglepattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 8; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
